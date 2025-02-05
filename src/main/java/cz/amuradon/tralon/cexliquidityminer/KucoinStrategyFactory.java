37f0e9ba05c676aebe75c101ba06e8aeef0d6021
Martin Augusta <martin.augusta@gmail.com@>
2025-02-05 10:27:43 +0100

    T #5 properties configuration

--- src/main/java/cz/amuradon/tralon/cexliquidityminer/KucoinStrategyFactory.java
+++ src/main/java/cz/amuradon/tralon/cexliquidityminer/KucoinStrategyFactory.java
@@ -1,5 +1,7 @@
 package cz.amuradon.tralon.cexliquidityminer;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import com.kucoin.sdk.KucoinPrivateWSClient;
 import com.kucoin.sdk.KucoinPublicWSClient;
 import com.kucoin.sdk.KucoinRestClient;
@@ -19,16 +21,39 @@ public class KucoinStrategyFactory {
     
 	private final KucoinPrivateWSClient wsClientPrivate;
 	
+	private final String baseToken;
+	
+	private final String quoteToken;
+	
+	private final int orderBookQuoteVolumeBefore;
+	
+	private final int maxQuoteBalanceToUse;
+	
+	private final int priceChangeDelayMs;
+	
 	@Inject
-    public KucoinStrategyFactory(final KucoinRestClient restClient, final KucoinPublicWSClient wsClientPublic,
-    		final KucoinPrivateWSClient wsClientPrivate) {
+    public KucoinStrategyFactory(final KucoinRestClient restClient,
+    		final KucoinPublicWSClient wsClientPublic,
+    		final KucoinPrivateWSClient wsClientPrivate,
+    		@ConfigProperty(name = "baseToken") String baseToken,
+    		@ConfigProperty(name = "quoteToken") String quoteToken,
+    		@ConfigProperty(name = "orderBookQuoteVolumeBefore") int orderBookQuoteVolumeBefore,
+    		@ConfigProperty(name = "maxQuoteBalanceToUse") int maxQuoteBalanceToUse,
+    		@ConfigProperty(name = "priceChangeDelayMs") int priceChangeDelayMs
+    ) {
 		this.restClient = restClient;
 		this.wsClientPublic = wsClientPublic;
 		this.wsClientPrivate = wsClientPrivate;
+		this.baseToken = baseToken;
+		this.quoteToken = quoteToken;
+		this.orderBookQuoteVolumeBefore = orderBookQuoteVolumeBefore;
+		this.maxQuoteBalanceToUse = maxQuoteBalanceToUse;
+		this.priceChangeDelayMs = priceChangeDelayMs;
     }
 	
 	@PostConstruct
 	public void init() {
-		new KucoinStrategy(restClient, wsClientPublic, wsClientPrivate, "VERSE", "USDT", 1000, 100, 5000).run();
+		new KucoinStrategy(restClient, wsClientPublic, wsClientPrivate, baseToken, quoteToken,
+				orderBookQuoteVolumeBefore, maxQuoteBalanceToUse, priceChangeDelayMs).run();
 	}
 }
