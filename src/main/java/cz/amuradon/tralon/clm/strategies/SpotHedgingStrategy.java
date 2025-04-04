9d4cd466ecafa385af39515327577111079da61c
Martin Augusta <martin.augusta@gmail.com@>
2025-04-04 13:43:26 +0200

    T #21 Preparation

--- src/main/java/cz/amuradon/tralon/clm/strategies/SpotHedgingStrategy.java
+++ src/main/java/cz/amuradon/tralon/clm/strategies/SpotHedgingStrategy.java
@@ -3,54 +3,65 @@ package cz.amuradon.tralon.clm.strategies;
 import java.math.BigDecimal;
 import java.util.Map;
 
-import cz.amuradon.tralon.clm.OrderBook;
 import cz.amuradon.tralon.clm.connector.OrderBookUpdate;
 import cz.amuradon.tralon.clm.connector.RestClient;
 import cz.amuradon.tralon.clm.connector.WebsocketClient;
 
 public class SpotHedgingStrategy implements Strategy {
+	
+	private final RestClient restClient;
+	private final WebsocketClient websocketClient;
+	private final String baseAsset;
+	private final String quoteAsset;
+	private final BigDecimal price;
+	private final BigDecimal baseQuantity;
 
 	public SpotHedgingStrategy(RestClient restClient, WebsocketClient websocketClient,
 			String baseAsset, String quoteAsset, BigDecimal price, BigDecimal baseQuantity) {
-		// TODO Auto-generated constructor stub
+		this.restClient = restClient;
+		this.websocketClient = websocketClient;
+		this.baseAsset = baseAsset;
+		this.quoteAsset = quoteAsset;
+		this.price = price;
+		this.baseQuantity = baseQuantity;
 	}
 
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
-		
+		// TODO
+		// subscribe trade (?) updates
+		//   * monitor price to place buy or sell order
+		// track order state - websocket or REST API?
+		// validate balance
+		// validate symbol
+		// validate price (might be already above)
 	}
 	
 	@Override
 	public void onOrderBookUpdate(OrderBookUpdate update, Map<BigDecimal, BigDecimal> orderBookSide) {
-		// TODO Auto-generated method stub
+		// Do nothing, not needed
 
 	}
 
 	@Override
 	public void onBaseBalanceUpdate(BigDecimal balance) {
-		// TODO Auto-generated method stub
+		// TODO Need to track?
 
 	}
 
 	@Override
 	public void onQuoteBalanceUpdate(BigDecimal balance) {
-		// TODO Auto-generated method stub
+		// TODO Need to track?
 
 	}
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
-		
+		// TODO
+		// close all opened orders
+		// sell asset?
 	}
-
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
-	}
-
+ 
 	@Override
 	public String getDescription() {
 		// TODO complete description
